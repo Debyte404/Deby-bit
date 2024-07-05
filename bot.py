50f83e5073c4a226f1190bddbf288e7309eedb4c
Ankit Chetri <163670260+Debyte404@users.noreply.github.com>
2024-07-06 04:02:43 +0530

    Update and rename api/bot.py to bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,15 @@
 from pyrogram import Client, filters
 from os import getenv
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 from shortner import shorten_url
 
 import logging
 import sys
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 #loads environment variables
-# load_dotenv()
+load_dotenv()
 
 #gets and assigns the env a variable
 API_ID = getenv('API_ID')
@@ -47,28 +45,4 @@ async def circumcised(client, message):
         print(e)
         await message.reply("Use it like this : /url www.example.com")
 
-# app.run()
-# Define the HTTP handler
-class handler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        try:
-            app.run()
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b'OK')
-        except Exception as e:
-            logger.error(f"Error handling POST request: {e}")
-            self.send_response(500)
-            self.end_headers()
-            self.wfile.write(b'Internal Server Error')
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-
-if __name__ == "__main__":
-    server = HTTPServer(('0.0.0.0', int(os.getenv('PORT', 8000))), handler)
-    logger.info("Starting server at port 8000")
-    server.serve_forever()
+app.run()
